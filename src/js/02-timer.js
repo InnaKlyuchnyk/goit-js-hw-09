7765df7bcf7ed5afbeedf88bcf413edacd0bea4d
Inna Klyuchnyk <Innysiaa@ukr.net>
2021-11-06 21:17:55 +0200

    Task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 const options = {
   enableTime: true,
@@ -7,9 +8,63 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates[0].getTime() < options.defaultDate.getTime()) {
+      Notiflix.Confirm.show('Ooops', 'Please choose a date in the future', 'Yes');
+    } else refs.startBtn.removeAttribute('disabled');
   },
 };
 
-const timerLib = flatpickr('input[type="text"]', options);
-console.log(timerLib);
+const timer = flatpickr('input[type="text"]', options);
+
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('button[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+console.log(refs.input);
+
+refs.startBtn.setAttribute('disabled', 'true');
+refs.startBtn.addEventListener('click', onStartBtnClick);
+
+function onStartBtnClick() {
+  refs.startBtn.setAttribute('disabled', 'true');
+  refs.input.setAttribute('disabled', 'true');
+
+  const startTime = timer.selectedDates[0].getTime();
+  setInterval(() => {
+    const currentTime = Date.now();
+    const deltaTime = startTime - currentTime;
+    const timeComponents = convertMs(deltaTime);
+    updateTimeInterface(timeComponents);
+    console.log(timeComponents);
+  }, 1000);
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateTimeInterface({ days, hours, minutes, seconds }) {
+  refs.days.textContent = days;
+  refs.hours.textContent = hours;
+  refs.minutes.textContent = minutes;
+  refs.seconds.textContent = seconds;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,70 @@
+const refs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+};
+
+refs.form.addEventListener('submit', createPromise);
+let position = 0;
+
 function createPromise(position, delay) {
+  event.preventDefault();
+  const amount = refs.amount.value;
+  const step = refs.step.value;
+  const firstDelay = refs.delay.value;
+
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  for (let i = 0; i <= amount; i += 1) {
+    position = i;
+    console.log(position);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    })
+      .then(() => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay} ms`);
+      })
+      .catch(() => {
+        console.log(`❌ Rejected promise ${position} in ${delay} ms`);
+      });
   }
 }
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+
+// function createPrimise() {
+//   event.preventDefault();
+//   const firstDelay = refs.delay.value;
+//   const amount = refs.amount.value;
+//   const step = refs.step.value;
+//   let delay = firstDelay;
+//   const shouldResolve = Math.random() > 0.3;
+
+//   for (let i = 1; i <= amount; i += 1) {
+//     const position = i;
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         if (shouldResolve) {
+//           resolve(i`✅Fulfilled promises ${position} in ${delay} ms`);
+//         } else {
+//           reject(i`❌Rejected promises ${position} in ${delay} ms`);
+//         }
+//       }, delay);
+//     })
+//       .then(v => console.log(v))
+//       .catch(error => console.log(error));
+//   }
+// }
